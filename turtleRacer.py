96e93feba15f6c8342d480f3e62741c4cc9f9681
Ivan Li <86083108+ivannnliii@users.noreply.github.com>
2023-03-12 21:31:43 -0400

    Add files via upload

---
+++ turtleRacer.py
@@ -0,0 +1,72 @@
+import turtle
+import time
+import random
+
+WIDTH, HEIGHT = 500, 500
+COLORS = ['Red', 'Green', 'Blue', 'Orange', 'Yellow', 'Black', 'Purple','Pink', 'Brown', 'Cyan']
+
+
+def get_number_of_racers():
+    racers = 0
+    while True:
+        racers = input("Enter the number of racers (2-10): ")
+        if racers.isdigit():
+            racers = int(racers)
+        else:
+            print("Input is not numeric... Try again!")
+            continue
+
+        if 2 <= racers <= 10:
+            return racers
+        else:
+            print("Number not in range 2-10. Try again!")
+
+
+def race(colors):
+    turtles = create_turtles(colors)
+
+    while True:
+        for racer in turtles:
+            distance = random.randrange(1, 20)
+            racer.forward(distance)
+
+            x, y = racer.pos()
+            if y >= HEIGHT // 2 - 10:
+                winner_color = colors[turtles.index(racer)]
+                winner_turtle = turtle.Turtle()
+                winner_turtle.hideturtle()
+                winner_turtle.penup()
+                winner_turtle.setpos(0, 0)
+                winner_turtle.write("Winner: {}".format(winner_color), align="center", font=("Arial", 24, "bold"))
+                time.sleep(5)
+                return winner_color
+
+
+def create_turtles(colors):
+    turtles = []
+    spacingx = WIDTH // (len(colors) + 1)
+    for i, color, in enumerate(colors):
+        racer = turtle.Turtle()
+        racer.color(color)
+        racer.shape('turtle')
+        racer.left(90)
+        racer.penup()
+        racer.setpos(-WIDTH//2 + (i + 1) * spacingx, -HEIGHT//2 + 20)
+        racer.pendown()
+        turtles.append(racer)
+
+    return turtles
+
+def init_turtle():
+    screen = turtle.Screen()
+    screen.setup(WIDTH, HEIGHT)
+    screen.title("Turtle Racer")
+
+
+racers = get_number_of_racers()
+init_turtle()
+
+random.shuffle(COLORS)
+colors = COLORS[:racers]
+
+winner = race(colors)
